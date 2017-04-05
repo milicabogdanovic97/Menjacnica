c05e0115b751b427662e7607bac40ba3cb48d79e
milicabogdanovic97 <milica.bogdanovic997@gmail.com>
2017-04-05 18:43:06 +0200

    Implementirane metode interfejsa na drugaciji nacin na master grani.

--- Menjacnica/src/specifikacijaInterfejsa/ImplementacijaKursMetode.java
+++ Menjacnica/src/specifikacijaInterfejsa/ImplementacijaKursMetode.java
@@ -1,27 +1,51 @@
 package specifikacijaInterfejsa;
 
 import java.util.GregorianCalendar;
+import java.util.LinkedList;
 
 import menjacnica.Valuta;
 
 public class ImplementacijaKursMetode implements KursMetode {
 
+	LinkedList<Valuta> kursnaLista = new LinkedList<Valuta>();
 	@Override
 	public void dodajKurs(String naziv, String skraceniNaziv, GregorianCalendar datum, double kupovniKurs,
 			double prodajniKurs, double srednjiKurs) {
-		// TODO Auto-generated method stub
-
+		Valuta novaValuta = new Valuta();
+		novaValuta.setNaziv(naziv);
+		novaValuta.setSkraceniNaziv(skraceniNaziv);
+		novaValuta.setDatumUnosaKursa(datum);
+		novaValuta.setKupovniKurs(kupovniKurs);
+		novaValuta.setProdajniKurs(prodajniKurs);
+		novaValuta.setSrednjiKurs(srednjiKurs);
+		
+		for (int i = 0; i < kursnaLista.size(); i++) {
+			if (novaValuta.equals(kursnaLista.get(i))) {
+				throw new RuntimeException("Kurs vec postoji.");
+			}
+		}
+		kursnaLista.add(novaValuta);
 	}
 
 	@Override
 	public void obrisiKurs(String naziv, GregorianCalendar datum) {
-		// TODO Auto-generated method stub
-
+		for (int i = 0; i < kursnaLista.size(); i++) {
+			Valuta v = kursnaLista.get(i);
+			if(v.getNaziv().equals(naziv) && v.getDatumUnosaKursa().getTime() == datum.getTime()){
+				kursnaLista.remove(i);
+				return;
+			}
+		}
 	}
 
 	@Override
 	public Valuta vratiKurs(String naziv, GregorianCalendar datum) {
-		// TODO Auto-generated method stub
+		for (int i = 0; i < kursnaLista.size(); i++) {
+			Valuta v = kursnaLista.get(i);
+			if(v.getNaziv().equals(naziv) && v.getDatumUnosaKursa().getTime() == datum.getTime()){
+				return kursnaLista.get(i);
+			}else throw new RuntimeException();
+		}
 		return null;
 	}
 
